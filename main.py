7a6c24d3c28920e5d9f55906e84e13caa704637e
Jhon Freddy Puentes <jhonpuentes93@hotmail.com>
2021-05-29 13:31:11 -0500

    :zap: Deteccion de palabrotas por pases

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from spanlp.palabrota import Palabrota
+from spanlp.domain.countries import Country
 import csv
 
 def leer_archivo(nombre_archivo:str='', separador_datos:str='', columna:str=''):
@@ -10,12 +11,12 @@ def leer_archivo(nombre_archivo:str='', separador_datos:str='', columna:str=''):
     return tweets
 
 
-def validar_vulgaridades(data:list=None):
+def validar_vulgaridades(data:list=None, pais:list=None):
     if data is None or len(data) == 0:
         raise Exception("Oiga! para validar vulgaridades debe enviar datos. esta enviando datos nulos o vacios.")
     
     contador_palabrotas = 0
-    validador = Palabrota()
+    validador = Palabrota(countries=pais)
     
     for texto in data:
         if isinstance(texto, str):
@@ -25,7 +26,26 @@ def validar_vulgaridades(data:list=None):
     return (contador_palabrotas>0,contador_palabrotas)
 
 
-comments = leer_archivo(nombre_archivo='tweets.csv', separador_datos='|', columna='comment_1')
-contiene, cantidad = validar_vulgaridades(comments)
+def pedir_pais():
+    print(""" Paises disponibles para el análisis: 
+            1 Colombia
+            2 Venezuela
+            3 Mexico
+            4 Argentina
+            5 Todos""")
+    pais_seleccionado = int(input("Ingrese:"))
+    return {
+        1: [Country.COLOMBIA],
+        2: [Country.VENEZUELA],
+        3: [Country.MEXICO],
+        4: [Country.ARGENTINA],
+        5: [Country.COLOMBIA, Country.VENEZUELA, Country.MEXICO, Country.ARGENTINA]
+    }.get(pais_seleccionado, None)
 
-print(f"Los datos contienen {cantidad} vulgaridades." if contiene else "Los datos no contienen vulgaridades.")
+
+comments = leer_archivo(nombre_archivo='data/tweets.csv', separador_datos='|', columna='comment_1')
+pais = pedir_pais()
+contiene, cantidad = validar_vulgaridades(comments, pais)
+
+print(f"Los datos contienen {cantidad} vulgaridades para {pais}." if contiene else "Los datos no contienen vulgaridades.")
+
